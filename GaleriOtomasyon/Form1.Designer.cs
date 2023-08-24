7ec8378b436dccaad552b3d74a2cc267be1acab1
Selahattin <selahattin0739@gmail.com>
2023-08-24 04:09:28 +0300

    proje yüklendi

---
+++ GaleriOtomasyon/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace GaleriOtomasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(351, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(12, 12);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(55, 14);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Araç No :";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(12, 54);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(38, 14);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Fiyat :";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl3.Location = new System.Drawing.Point(12, 97);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(39, 14);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "Adet :";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl5.Location = new System.Drawing.Point(12, 175);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(47, 14);
+            this.lbl5.TabIndex = 5;
+            this.lbl5.Text = "Model :";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl4.Location = new System.Drawing.Point(12, 134);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(47, 14);
+            this.lbl4.TabIndex = 6;
+            this.lbl4.Text = "Marka :";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(84, 12);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 7;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(84, 54);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 8;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(84, 97);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 9;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(84, 134);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 20);
+            this.txt4.TabIndex = 10;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(84, 175);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 20);
+            this.txt5.TabIndex = 11;
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(452, 171);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(240, 20);
+            this.bt1.TabIndex = 12;
+            this.bt1.Text = "Listele";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(452, 210);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 13;
+            this.btn2.Text = "Kaydet";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(533, 210);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 14;
+            this.btn3.Text = "Yenile";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(617, 210);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 15;
+            this.btn4.Text = "Sil";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 280);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+    }
+}
+
---
+++ GaleriOtomasyon/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaleriOtomasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DB_GaleriEntities Baglantı = new DB_GaleriEntities();
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            Goruntuleme();
+            txt1.Visible = false;
+            lbl1.Visible = false;
+        }
+        public void Goruntuleme()
+        {
+            dataGridView1.DataSource = Baglantı.Araclars.ToList();
+        }
+      
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Araclar save = new Araclar();
+            save.AracFiyat = Convert.ToDecimal(txt2.Text);
+            save.Marka = txt4.Text;
+            save.Adet = Convert.ToInt32(txt3.Text);
+            save.Model = txt5.Text;
+            Baglantı.Araclars.Add(save);
+            Baglantı.SaveChanges();
+            Goruntuleme();
+
+            
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow satır = dataGridView1.CurrentRow;
+            txt1.Text = satır.Cells["AracNo"].Value.ToString();
+            txt2.Text = satır.Cells["AracFiyat"].Value.ToString();
+            txt3.Text = satır.Cells["Adet"].Value.ToString();
+            txt4.Text = satır.Cells["Marka"].Value.ToString();
+            txt5.Text = satır.Cells["Model"].Value.ToString();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            int no = Convert.ToInt32(txt1.Text);
+            var update = Baglantı.Araclars.Where(x => x.AracNo == no).FirstOrDefault();
+            update.AracFiyat = Convert.ToDecimal(txt2.Text);
+            update.Marka = txt4.Text;
+            update.Adet = Convert.ToInt32(txt3.Text);
+            update.Model = txt5.Text;
+            Baglantı.SaveChanges();
+            Goruntuleme();
+            
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            int no = Convert.ToInt32(txt1.Text);
+            var delete = Baglantı.Araclars.Where(a=>a.AracNo ==no).FirstOrDefault();
+            Baglantı.Araclars.Remove(delete);
+            Baglantı.SaveChanges();
+            Goruntuleme();
+        }
+    }
+}
